eb7c33400a1858aac584e1ce811e9cbbd41340ed
JahedHossenBangladesh <jahedhossen4358.com>
2021-02-01 01:50:47 +0600

    add json in header

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require("http");
+const todos = [
+  { id: 1, text: "todo 1" },
+  { id: 2, text: "todo 2" },
+  { id: 3, text: "todo 3" },
+];
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
-  console.log(headers, url, method);
-  res.end();
+  //   const { headers, url, method } = req;
+  //   console.log(headers, url, method);
+  res.setHeader("Content-type", "application/json");
+  res.setHeader("X-powerd-by", "node.js");
+
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 const PORT = 5000;
 
-server.listen(PORT, () => console.log(`ok server ${PORT}`));
+server.listen(PORT, () => console.log(`ok svr ${PORT}`));
